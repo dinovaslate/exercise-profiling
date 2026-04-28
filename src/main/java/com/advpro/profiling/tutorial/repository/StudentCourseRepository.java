77d956a302421f5f5681aff6cccd8f20dbce3ac1
dinovaslate <dinovasala3@gmail.com>
2026-04-28 16:16:10 +0700

    [Refactoring] - Optimize all student query

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentCourseRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentCourseRepository.java
@@ -1,7 +1,7 @@
 package com.advpro.profiling.tutorial.repository;
 
-import com.advpro.profiling.tutorial.model.Student;
 import com.advpro.profiling.tutorial.model.StudentCourse;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +13,7 @@ import java.util.List;
 @Repository
 public interface StudentCourseRepository extends JpaRepository<StudentCourse, Long> {
     List<StudentCourse> findByStudentId(Long studentId);
+
+    @Query("select sc from StudentCourse sc join fetch sc.student join fetch sc.course order by sc.student.id, sc.id")
+    List<StudentCourse> findAllWithStudentAndCourse();
 }
